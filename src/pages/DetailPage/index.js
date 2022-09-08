e3ab648da00b96fb8ea0d7ebca11c835c150c1e2
Park Jun Beom <wnsqja6800@naver.com>
2022-09-09 01:07:54 +0900

    initial

---
+++ src/pages/DetailPage/index.js
@@ -0,0 +1,33 @@
+import axios from '../../api/axios';
+import React, { useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+
+
+export default function DetailPage() {
+
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState({})
+  console.log("movieId",movieId)
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const requset = await axios.get (
+       `/movie/${movieId}`
+      )
+      setMovie(requset.data)
+    }
+    fetchData();
+  },[movieId])
+
+if(!movie) return <div>...loading</div>;
+
+  return (
+    <section>
+      <img 
+      className='modal__poster-img'
+      src = {`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+      alt="poster"/>
+    </section>
+  )
+}
